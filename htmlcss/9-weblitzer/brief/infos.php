55b35098bca5d032f34eb49c17edb351dc54f754
quidelantoine <quidelantoine@gmail.com>
2022-10-14 10:46:48 +0200

    day 5 2

---
+++ htmlcss/9-weblitzer/brief/infos.php
@@ -0,0 +1,21 @@
+<?php
+
+// infos
+//.wrap => max-width: 1040px;
+//.wrap2  => max-width: 840px;
+
+/////////////
+// TYPO
+/////////////
+// Titillium+Web => Google font
+
+/////// COLOR
+// violetfonce =>  #87509c;
+// violetclair =>  #643a79;
+// orange      =>  #eb7d4b
+// vertfonce =>  #14a58c;
+// vertclair =>  #17c2a4;
+// bleufonce =>  #273a71;
+// bleuclair =>  #3c5499;
+// rouge    => red
+
