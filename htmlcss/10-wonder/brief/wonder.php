5c7c83a72471f1d50ad0df73048784f86f253d12
quidelantoine <quidelantoine@gmail.com>
2022-10-17 09:35:41 +0200

    add brief wonder2

---
+++ htmlcss/10-wonder/brief/wonder.php
@@ -0,0 +1,14 @@
+<?php
+/////////////
+//   TYPO
+///////////
+// Arvo  => 400, 700
+// font-family: 'Arvo', serif;
+// font-size: 17px;
+// line-height: 1.5;
+// font-weight: 400;
+
+/////////////
+//   WRAP
+///////////
+//  960px
