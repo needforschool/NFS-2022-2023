3b4228223036a2782549ad2afd90a13c4d8517a1
quidelantoine <quidelantoine@gmail.com>
2022-10-18 09:12:53 +0200

    add brief burger

---
+++ htmlcss/11-burger/brief/brief.php
@@ -0,0 +1,8 @@
+<?php
+
+//  body { font:500 15px/1.4 'Montserrat', sans-serif;color:#3b3b3b; background-color: #fff; }
+
+// .wrap {max-width: 1440px;width:100%;margin: 0 auto;padding: 0 1rem;}
+//.wrap2 {max-width: 1324px;width:100%;margin: 0 auto;padding: 0 1rem;}
+//.wrap3 {max-width: 1235px;width:100%;margin: 0 auto;padding: 0 1rem;}
+//.wrap4 {max-width: 785px;width:100%;margin: 0 auto;padding: 0 1rem;}
