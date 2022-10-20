06e4d86b19311b829f36b4fba388135b5518088f
quidelantoine <quidelantoine@gmail.com>
2022-10-20 16:48:47 +0200

    day 2 php 8

--- php/php5/index.php
+++ php/php5/index.php
@@ -28,3 +28,101 @@ print_r($arrayDeOuf);
 echo '</pre>';
 
 echo $arrayDeOuf[2]['sdfsd'][2390]['dede'];
+///////////////////////
+/// Tableau multidimensionnel
+///////////////////////
+$paiements = array(
+    array(
+        'montant' => 12.55,
+        'date'    => '12/12/2022'
+    ),
+    array(
+        'montant' => 14.75,
+        'date'    => '13/12/2022'
+    ),
+    array(
+        'montant' => 2.25,
+        'date'    => '14/12/2022'
+    ),
+);
+
+// Affichez le montant du premier paiement
+echo $paiements[0]['montant'];
+// Affichez la date du dernier paiement
+echo $paiements[2]['date'];
+// Ajoutez un nouveau paiement
+$paiements[] = array(
+    'montant' => 34.63,
+    'date'    => '12/12/12'
+);
+echo '<pre>';
+print_r($paiements);
+echo '</pre>';
+// for  &  foreach
+foreach($paiements as $key => $paiement) {
+    echo '<p>'.$key.' Le paiement de ' . $paiement['montant'] . ' à la date du '.$paiement['date'].'</p>';
+}
+echo '<hr>';
+for($i = 0; $i < count($paiements); $i++) {
+    echo '<p>'.$i.' Le paiement de ' . $paiements[$i]['montant'] . ' à la date du '.$paiements[$i]['date'].'</p>';
+}
+$users = [
+    ['nom' => 'Michel','mail' => 'michel@gmail.com','age' => 35],
+    ['nom' => 'Jacky','mail' => 'jacky@gmail.com','age' => 25],
+    ['nom' => 'Bernard','mail' => 'bernard@gmail.com','age' => 55],
+    ['nom' => 'Janine','mail' => 'Janine@gmail.com','age' => 32],
+];
+// for // foreach
+// section id="users"
+    // chaque user
+        // div => class="one_user"
+            // h3 => nom
+            // p => a => mailto
+            // p => age => class="user_age"
+
+echo '<section id="users">';
+for($i = 0;$i < count($users); $i++) {
+    echo '<div class="one_user">';
+        echo '<h2>'.$users[$i]['nom'].'</h2>';
+        echo '<p><a href="mailto:'.$users[$i]['mail'].'">'.$users[$i]['mail'].'</a></p>';
+        echo '<p class="user_age">Age: '.$users[$i]['age'].'</p>';
+    echo '</div>';
+}
+echo '</section>';
+echo '<hr>';
+echo '<section id="users">';
+foreach ($users as $user) {
+    echo '<div class="one_user">';
+        echo '<h2>'.$user['nom'].'</h2>';
+        echo '<p><a href="mailto:'.$user['mail'].'">'.$user['mail'].'</a></p>';
+        echo '<p class="user_age">Age: '.$user['age'].'</p>';
+    echo '</div>';
+}
+echo '</section>';
+
+?>
+<hr>
+<section id="users">
+    <?php for($i = 0;$i < count($users); $i++) { ?>
+        <div class="one_user">
+            <h2><?php echo $users[$i]['nom']; ?></h2>
+            <p><a href="mailto:<?php echo $users[$i]['mail']; ?>"><?php echo $users[$i]['mail']; ?></a></p>
+            <p class="user_age"><?php echo $users[$i]['age']; ?></p>
+        </div>
+    <?php } ?>
+</section>
+
+<hr>
+<section id="users">
+    <?php foreach ($users as $user) { ?>
+        <div class="one_user">
+            <h2><?php echo $user['nom']; ?></h2>
+            <p><a href="mailto:<?php echo $user['mail']; ?>"><?php echo $user['mail']; ?></a></p>
+            <p class="user_age"><?php echo $user['age']; ?></p>
+        </div>
+    <?php } ?>
+</section>
+
+
+
+
