6456d2e39201e0fe528961fbdb171be5341d23d6
quidelantoine <quidelantoine@gmail.com>
2022-10-21 14:43:08 +0200

    day 3 php - 5

--- php/php7/index.php
+++ php/php7/index.php
@@ -10,6 +10,7 @@ $title = 'Mon site - Page home';
 //echo '<pre>';
 //print_r($_GET);
 //echo '</pre>';
+echo '$_POST';
 echo '<pre>';
 print_r($_POST);
 echo '</pre>';
@@ -22,19 +23,49 @@ echo '</pre>';
 //    echo $prenom;
 //}
 
-if(!empty($_POST['nom'])) {
-    $nom = $_POST['nom'] . ' ';
-    echo $nom;
-}
-if(!empty($_POST['prenom'])) {
-    $prenom = $_POST['prenom'];
-    echo $prenom;
-}
-// Soumis
-// xss
-// $errors $success
-// VAlidation
+//if(!empty($_POST['nom'])) {
+//    $nom = $_POST['nom'] . ' ';
+//    echo $nom;
+//}
+//if(!empty($_POST['prenom'])) {
+//    $prenom = $_POST['prenom'];
+//    echo $prenom;
+//}
 
+
+if(!empty($_POST['submitted2'])) {
+    die('ok submitted 2');
+}
+
+$errors = array();
+// Soumis
+// if formulaire est soumis ??
+if(!empty($_POST['submitted'])) {
+    // Faille XSS
+    $nom = trim(strip_tags($_POST['nom']));
+    $prenom = trim(strip_tags($_POST['prenom']));
+    // Validation
+    // Validation champ $nom
+    // n'est pas vide
+        // mb_strlen
+        // Si $nbre < 3
+            //$errors['nom'] = 'Veuillez renseigner au moins 3 caractère';
+        // SINON Si $nbre > 70
+
+        // else
+            //no error
+    // vide
+        //$errors['nom'] = 'Veuillez renseigner ce champ';
+
+    //$errors['nom'] = 'Veuillez renseigner ce champ';
+    //$errors['prenom'] = 'Veuillez renseigner ce champ';
+
+
+}
+echo '$errors';
+echo '<pre>';
+print_r($errors);
+echo '</pre>';
 
 include('inc/header.php'); ?>
 
@@ -42,11 +73,17 @@ include('inc/header.php'); ?>
     <form action="" method="post">
         <label for="nom">Nom * (min 3, max 70)</label>
         <input type="text" id="nom" name="nom" value="<?php if(!empty($_POST['nom'])) { echo $_POST['nom']; } ?>">
+        <span class="error"><?php if(!empty($errors['nom'])) { echo $errors['nom']; } ?></span>
 
-        <label for="nom">Prénom * (min 5, max 100)</label>
+        <label for="prenom">Prénom * (min 5, max 100)</label>
         <input type="text" id="prenom" name="prenom" value="<?php if(!empty($_POST['prenom'])) { echo $_POST['prenom']; } ?>">
+        <span class="error"><?php if(!empty($errors['prenom'])) { echo $errors['prenom']; } ?></span>
 
-        <input type="submit">
+        <input type="submit" name="submitted" value="Envoyer">
+    </form>
+
+    <form action="" method="post">
+        <input type="submit" name="submitted2" value="Envoyer">
     </form>
 </section>
 
