feb4c6f2f9d3d7626d134b4543aaf23315e1d184
quidelantoine <quidelantoine@gmail.com>
2022-10-21 09:25:02 +0200

    day 3 php - 1

---
+++ php/php6/exemple.php
@@ -0,0 +1,4 @@
+<?php
+
+$str = '<p>lkjlkjlj</p>';
+$str2 = strip_tags($str); // lkjlkjlj
---
+++ php/php6/index.php
@@ -0,0 +1,49 @@
+<?php
+// echo "<br />";  // pour un retour à la ligne
+// Manuel seulement ! Internet interdit !
+// ne pas réinventer la roue, fonction existe sûrement déjà !!!
+$fable = "Maître corbeau sur son arbre perché tenait en son bec un fromage";
+
+//Calculez le nombre de caractères dans la chaîne ci-dessous et stockez-la dans une variable nommée "myStringLength" :
+
+//Affichez la variable myStringLength
+
+//calculez le nombre de mots dans la phrase précédente et affichez le
+
+//affichez la chaîne toute en majuscule
+
+//affichez la chaîne avec seulement la première lettre de la phrase en majuscule
+
+//affichez la chaîne avec la première lettre de chaque mot en majuscule
+
+//affichez le 10 premiers caractères de la chaîne
+
+//affichez la chaîne à l'envers (le dernier caractère en premier)
+
+//remplacez tous les "a" par des "?"
+
+//envoyez-vous la chaîne par email, avec la fonction mail()
+
+//affichez la valeur de pi (π) à l'écran (une fonction existe)
+
+//arrondissez la valeur de pi à 2 décimales et affichez la
+
+//générez un nombre aléatoire entre 10 et 20, et affichez le
+
+//stockez tous les mots de la chaîne $fable dans un array nommé "words"
+
+//affichez le premier mot de votre array
+
+//affichez le deuxième mot de votre array
+
+//affichez le dernier mot de votre array, sans compter le nombre d'éléments (une fonction spécifique existe)
+
+//essayez la fonction suivante : print_r($words);
+
+//Reformez une chaîne à partir de votre array. Chaque mot doit être recollés avec un "<br />". Stockez cette chaîne dans une variable nommée "fable2"
+
+//Affichez l'année actuelle
+
+//Affichez la date au format suivant : jj/mm/yyyy
+
+//affichez tous les paramètres de configuration de PHP dans votre page (une fonction existe pour ça)
