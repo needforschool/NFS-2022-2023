4a50dbb92740d4ed4733f6f02917bb63f1aaf575
quidelantoine <quidelantoine@gmail.com>
2022-10-21 12:18:28 +0200

    day 3 php - 3

---
+++ php/php7/contact.php
@@ -0,0 +1,10 @@
+<?php
+
+$title = 'Mon site - Page contact';
+include('inc/header.php'); ?>
+
+<section>
+    <h1>Contact</h1>
+</section>
+
+<?php include('inc/footer.php');
---
+++ php/php7/index.php
@@ -0,0 +1,12 @@
+<?php
+$title = 'Mon site - Page home';
+
+include('inc/header.php'); ?>
+
+<section>
+    <h1>home</h1>
+
+
+</section>
+
+<?php include('inc/footer.php');
