6900645a9e2b005cc2af6058c3c049ff264ff601
quidelantoine <quidelantoine@gmail.com>
2022-10-24 10:38:16 +0200

    day 4 php 2

--- php/php8/inc/function.php
+++ php/php8/inc/function.php
@@ -17,3 +17,43 @@ function debug($tableau) {
     print_r($tableau);
     echo '</pre>';
 }
+
+// LES VARIABLES SONT LOCALES A LA FONCTION
+$pdo = 'bonjour';
+$var = 12;
+function testVariable($b) {
+    global $pdo;
+    echo $pdo;
+    echo $b;
+}
+testVariable($var);
+
+// String => toutes les lettres en minuscule et ensuite mettre la chaine avec uniquement la première lettre en majuscule
+function maj($str) {
+//    $str = strtolower($str);
+//    $str = ucfirst($str);
+//    return $str;
+    return ucfirst(mb_strtolower($str));
+}
+// strtolower
+// ucfirst
+echo maj('GJIJk ljlkjd iopk ljLKJLKf gjhLMKLM');
+
+// minus
+    // tout en majuscule , la premiere lettre en minuscule
+function minus($str) {
+    return lcfirst(mb_strtoupper($str));
+}
+echo minus('GJIJk ljlkjd iopk ljLKJLKf gjhLMKLM');
+
+
+// EXO
+$plats = array('Tartiflette', 'pot au feu', ' mousse au chocolat', ' blanquette', 'fondue', 'raclette','Boeuf','quiche lorraine', 'steak tartare', 'banane flambée');
+
+$defaults = array('avare','paresseux','ennuyant','borné','cruel','versatile','ringard','stupide','superficiel','egoiste');
+// array_rand
+// créez un fonction 'generateGroupName'
+    // Prendre au hasrad un element du premier tableau et du deuxieme tableau
+    // former un phrase " Tartiflette frustrée"
+    // div => ecrit en rose , chaque mot doit commencer une majuscule
+
