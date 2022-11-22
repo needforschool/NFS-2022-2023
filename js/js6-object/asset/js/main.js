139e00a16158cd672529ac3138b6a0f268ee5def
quidelantoine <quidelantoine@gmail.com>
2022-11-22 16:59:01 +0100

    day 2 js 6

--- js/js6-object/asset/js/main.js
+++ js/js6-object/asset/js/main.js
@@ -89,6 +89,90 @@ const {g,h, ...restEncore} = obj4;
 console.log({g,h,restEncore});
 
 // Tester l'existence des propriétés
+const gg = {
+    a: 23
+};
+if(gg.a) {
+    console.log(gg.a)
+}
+if(gg.b) {
+    console.log('Invisible');
+}
+if(punto.km) {
+    console.log(punto.km);
+}
+// fusionner des objects
+const obj1 = {a:1,b:2};
+const obj2 = {c:3};
+let fusion = Object.assign({}, obj1,obj2);
+console.log(fusion); // {a: 1, b: 2, c: 3}
+const obj3 = {a: 'chaine'}
+const obj5 = {b: 'hjgdjhzgjhs'}
+let fusion2 = Object.assign({},obj1,obj2,obj3,obj5);
+console.log(fusion2);
+
+// Fusionner des object avec SPREAD ...
+const fusion3 = {...obj1, ...obj2}; //  {a: 1, b: 2, c: 3}
+console.log(fusion3);
+// Copier un object
+let original = {a: 3};
+let copy = original;
+console.log({original, copy});
+copy.a = 15;
+console.log({original, copy});
+// ici on modifie aussi l'original
+// les objects sont copiés par référence
+// GoodWay
+let original2 = { a: 3};
+let copy2 = { ...original2 }
+console.log({original2, copy2});
+copy2.a = 15;
+console.log({original2, copy2});
+// Comparer des objects
+console.log({} === {}); // false
+console.log(original === copy); // true
+
+// Itérer sur un object
+const user = {
+    name: 'Antoine',
+    age: 14
+}
+
+for(propriete in user) {
+    console.log(propriete)
+    console.log(user[propriete])
+}
+// Object.values()
+const user2 = Object.values(user);
+console.log(user2); // ['Antoine', 14]
+// Object.keys()
+const user3 = Object.keys(user);
+console.log(user3); // ['name', 'age']
+// Object.entries
+const user4 = Object.entries(user);
+console.log(user4); // [['name', 'Antoine'],['age', 14]]
+console.log(user4[1][1]);
+
+// JSON
+let norris = '{"categories":[],"created_at":"2020-01-05 13:42:19.324003","icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"55d0aCZ9TOejWz68jUa4vg","updated_at":"2020-01-05 13:42:19.324003","url":"https://api.chucknorris.io/jokes/55d0aCZ9TOejWz68jUa4vg","value":"This year\'s Chuck Norris Super Sunday Super Bowl party was catered by Chick-fil-A and all other party items delivered by Hobby Lobby"}'
+console.log(norris)
+// JSON => Object
+let norrisObject = JSON.parse(norris);
+console.log(norrisObject);
+document.write(norrisObject.value);
+
+// Object => JSON
+console.log(JSON.stringify(punto));
+
+// copier une Object => BEST WAY
+const bobo = {a:1,b:2}
+const copyBobo = JSON.parse(JSON.stringify(bobo));
+copyBobo.a = 12;
+console.log({bobo, copyBobo});
+
+// cloneDeep()
+
+
 
 
 
