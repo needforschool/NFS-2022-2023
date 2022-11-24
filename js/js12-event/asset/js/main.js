28abd1e19c1fa473d468a97e40c491c498316d51
quidelantoine <quidelantoine@gmail.com>
2022-11-24 15:43:27 +0100

    day 4 js 3

--- js/js12-event/asset/js/main.js
+++ js/js12-event/asset/js/main.js
@@ -32,8 +32,41 @@ infos.addEventListener('mouseleave', (e) => {
     console.log('ok je sort');
     infos.style.backgroundColor = 'gainsboro';
 })
+// infos2
 
+const paragraphes = document.querySelectorAll('#infos2 p');
+console.log(paragraphes);
 
+// for(let i = 0; i < paragraphes.length; i++) {
+//     paragraphes[i].addEventListener('click', function() {
+//         this.style.color = 'red'
+//     });
+// }
+paragraphes.forEach((p) => {
+    p.addEventListener('click', function() {
+        if(p.dataset.color) {
+            //console.log('color ok')
+            this.style.color = p.dataset.color
+        } else {
+            //console.log('color pas ok');
+            this.style.color = 'red'
+        }
+
+       // this.style.color = 'red'
+    });
+});
+
+const boite = document.querySelector('#boite');
+boite.addEventListener('click', function() {
+    //boite.classList.toggle('open')
+    if(boite.classList.contains('open')) {
+        boite.classList.remove('open')
+        boite.innerText = '';
+    } else {
+        boite.classList.add('open');
+        boite.innerText = 'Le texte'
+    }
+})
 
 
 
