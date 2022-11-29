8503d5357395adbba8e6d19f3c3f8fa3f5e9333f
quidelantoine <quidelantoine@gmail.com>
2022-11-29 12:36:56 +0100

    day 6 js 2

--- js/js17-exo-html/asset/js/main.js
+++ js/js17-exo-html/asset/js/main.js
@@ -1,1 +1,16 @@
 console.log('js 17');
+
+// burger
+
+const box_burger = document.querySelector('#menu_burger');
+const btn_open = document.querySelector('#opener_burger');
+const btn_close = box_burger.querySelector('#closer');
+btn_open.addEventListener('click', function(e) {
+    e.preventDefault();
+    box_burger.classList.add('opened');
+});
+
+btn_close.addEventListener('click', function(e) {
+    e.preventDefault();
+    box_burger.classList.remove('opened');
+})
